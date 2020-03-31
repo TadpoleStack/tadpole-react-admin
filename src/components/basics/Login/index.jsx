cdae8c7afedda125018122f08c233254f6f1cc33
chensiyang <chensiyang@buzzly.cn>
2020-03-31 15:54:06 +0800

    修改

--- src/components/basics/Login/index.jsx
+++ src/components/basics/Login/index.jsx
@@ -1,54 +1,87 @@
 import React, { Component } from 'react'
 import './index.scss'
-import { Form, Input, Button, message } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button, message } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import loadable from 'utils/loadable'
 const WebGLbg = loadable(() => import('../WebGLbg'))
 const Timer = loadable(() => import('../Timer'))
 
 class Login extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         Timer: null
-      }
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            Timer: null,
+        }
+    }
 
-   handleSubmit(values) {
-      if (values.username === 'Tadpole' && values.password === 'admin') {
-         message.success('登录成功！')
-         this.props.history.push('/home')
-      } else message.error('登录失败！用户名和密码不匹配')
-
-   };
-   //阻止默认
-   prevetDefault(e) {
-      e.preventDefault()
-   }
-   render() {
-      return (
-         <div className="login-wrap" onContextMenu={this.prevetDefault}>
-            <WebGLbg ></WebGLbg>
-            <Timer width={window.innerWidth / 3} height={window.innerHeight}></Timer>
-            <div className="login-form-wrap" style={{ width: window.innerWidth < 992 ? '280px' : '360px' }}>
-               <h2 align="center">Tadpole React Admin</h2>
-               <Form onFinish={this.handleSubmit.bind(this)}>
-                  <Form.Item name="username" rules={[{ required: true, message: '请输入用户名!' }]}>
-                     <Input prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        placeholder="用户名 Tadpole"
-                     />
-                  </Form.Item>
-                  <Form.Item name="password" rules={[{ required: true, message: '请填写密码!' }]}>
-                     <Input.Password prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密码 admin" />
-                  </Form.Item>
-                  <Form.Item>
-                     <Button block={true} type="primary" htmlType="submit">登录</Button>
-                  </Form.Item>
-               </Form>
+    handleSubmit(values) {
+        if (values.username === 'Tadpole' && values.password === 'admin') {
+            message.success('登录成功！')
+            this.props.history.push('/home')
+        } else message.error('登录失败！用户名和密码不匹配')
+    }
+    //阻止默认
+    prevetDefault(e) {
+        e.preventDefault()
+    }
+    render() {
+        return (
+            <div className="login-wrap" onContextMenu={this.prevetDefault}>
+                <WebGLbg></WebGLbg>
+                <Timer
+                    width={window.innerWidth / 3}
+                    height={window.innerHeight}
+                ></Timer>
+                <div
+                    className="login-form-wrap"
+                    style={{
+                        width: window.innerWidth <= 1024 ? '280px' : '360px',
+                    }}
+                >
+                    <h2 align="center">Tadpole React Admin</h2>
+                    <Form onFinish={this.handleSubmit.bind(this)}>
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                { required: true, message: '请输入用户名!' },
+                            ]}
+                        >
+                            <Input
+                                prefix={
+                                    <UserOutlined
+                                        style={{ color: 'rgba(0,0,0,.25)' }}
+                                    />
+                                }
+                                placeholder="用户名 Tadpole"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: '请填写密码!' }]}
+                        >
+                            <Input.Password
+                                prefix={
+                                    <LockOutlined
+                                        style={{ color: 'rgba(0,0,0,.25)' }}
+                                    />
+                                }
+                                placeholder="密码 admin"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                block={true}
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                登录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
-         </div >
-      )
-   }
+        )
+    }
 }
 
 export default Login
--- src/components/business/Home/index.jsx
+++ src/components/business/Home/index.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from 'react'
-import {
-   HashRouter as Router,
-   Switch,
-   Route,
-   Redirect
-} from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 // import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import loadable from 'utils/loadable'
 import './index.scss'
@@ -16,40 +11,67 @@ const TypedPlugin = loadable(() => import('components/business/TypedPlugin'))
 const IconPage = loadable(() => import('components/business/UI/IconPage'))
 
 class Home extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         headerHeight: '60px',
-         sidebarWidth: '260px'
-      }
-   }
+    constructor(props) {
+        super(props)
+        this.state = {
+            windowWidth: window.innerWidth + 'px',
+            headerHeight: '60px',
+            sidebarWidth: '260px',
+        }
+    }
 
-
-   render() {
-      return (
-         <div style={{ width: '100%', height: '100%' }}>
-            <Header height={this.state.headerHeight} />
-            <div style={{ width: '100%', height: `calc(100% - ${this.state.headerHeight})`, display: 'flex' }}>
-               <Sidebar width={this.state.sidebarWidth} />
-               <div style={{ height: '100%', flex: 1, overflowY: 'hidden' }}>
-                  {/* <TransitionGroup>
+    componentWillMount() {}
+    render() {
+        return (
+            <div style={{ width: '100%', height: '100%' }}>
+                <Header height={this.state.headerHeight} />
+                <div
+                    style={{
+                        width: '100%',
+                        height: `calc(100% - ${this.state.headerHeight})`,
+                        display: 'flex',
+                    }}
+                >
+                    <Sidebar width={this.state.sidebarWidth} />
+                    <div
+                        style={{ height: '100%', flex: 1, overflowY: 'hidden' }}
+                    >
+                        {/* <TransitionGroup>
                      <CSSTransition classNames="fade" key={this.props.location.pathname} timeout={500}> */}
-                  <Router>
-                     <Switch>
-                        <Route exact path="/home" component={Start}></Route>
-                        <Route exact path="/home/iconpage" component={IconPage}></Route>
-                        <Route exact path="/home/typedplugin" component={TypedPlugin}></Route>
-                        <Route exact path="/home/404" component={NotFound}></Route>
-                        <Route render={() => <Redirect to="/home/404" />} />
-                     </Switch>
-                  </Router>
-                  {/* </CSSTransition>
+                        <Router>
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/home"
+                                    component={Start}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/home/iconpage"
+                                    component={IconPage}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/home/typedplugin"
+                                    component={TypedPlugin}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/home/404"
+                                    component={NotFound}
+                                ></Route>
+                                <Route
+                                    render={() => <Redirect to="/home/404" />}
+                                />
+                            </Switch>
+                        </Router>
+                        {/* </CSSTransition>
                   </TransitionGroup> */}
-               </div>
+                    </div>
+                </div>
             </div>
-         </div >
-      )
-   }
+        )
+    }
 }
 
 export default Home
