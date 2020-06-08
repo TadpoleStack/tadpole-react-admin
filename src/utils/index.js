22c2ad9046c43b95c009e6d17a69fe42a8842574
chensiyang <chensiyang@buzzly.cn>
2020-06-08 17:22:41 +0800

    调整2

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,8 @@
  */
 import * as code from './code'
 import * as EventEmitter from './EventEmitter'
-const global_method = {...code,...EventEmitter}
+import * as loadable from './loadable'
+const global_method = {...code,...EventEmitter,...loadable}
 
 const addGlobalMethods = React=>{
    for (const key in global_method) {
--- src/utils/loadable.js
+++ src/utils/loadable.js
@@ -5,7 +5,7 @@ import Loadable from 'react-loadable'
 import Loading from 'components/basics/Loading'
 
 //通用的Loading组件默认使用Loading
-export default (loader, loading = Loading) => {
+export const loadable = (loader, loading = Loading) => {
     return Loadable({
         loader,
         loading,
