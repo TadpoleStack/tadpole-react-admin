b9891fc3718613abc934dd874ae68b0f3307e9bd
chensiyang <chensiyang@buzzly.cn>
2020-06-11 09:00:28 +0800

    ***

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,6 @@
 const CracoAlias = require("craco-alias");
 const CracoAntDesignPlugin = require("craco-antd");
 const sassResourcesLoader = require('craco-sass-resources-loader');
-const imageOptimizer = require('craco-image-optimizer-plugin');
 
 module.exports = {
    plugins: [
@@ -24,28 +23,6 @@ module.exports = {
          options: {
             resources: 'public/global/variable.scss',
          },
-      },
-      {
-         plugin: imageOptimizer,
-         options: {
-           mozjpeg: {
-             progressive: true,
-             quality: 65,
-           },
-           optipng: {
-             enabled: false,
-           },
-           pngquant: {
-             quality: [0.65, 0.9],
-             speed: 4,
-           },
-           gifsicle: {
-             interlaced: false,
-           },
-           webp: {
-             quality: 75,
-           },
-         },
-       }
+      }
    ]
 }
